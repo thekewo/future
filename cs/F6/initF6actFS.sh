c26796744a1aee0cde692842d002ce88f1092c3a
Norbert Batfai <nbatfai@gmail.com>
2018-02-10 15:45:19 +0100

    f2-f6

---
+++ cs/F6/initF6actFS.sh
@@ -0,0 +1,21 @@
+mkdir City
+mkdir City/Debrecen
+mkdir City/Debrecen/Sport
+mkdir City/Debrecen/Sport/Esport
+mkdir City/Debrecen/Sport/Esport/DEAC-Hackers
+echo "Belbecs/Tapasztalat/Közösségi 80" >> City/Debrecen/Sport/Esport/DEAC-Hackers/DEAC-Hackers.props
+
+mkdir City/Debrecen/Sport/Esport/DEAC-Hackers/Verseny
+mkdir City/Debrecen/Sport/Esport/DEAC-Hackers/Verseny/LoL
+
+echo "Belbecs/Képesség/Térelképzelés 15" >> City/Debrecen/Sport/Esport/DEAC-Hackers/Verseny/LoL/LoL.props
+echo "Belbecs/Képesség/Reakcióidő 80" >> City/Debrecen/Sport/Esport/DEAC-Hackers/Verseny/LoL/LoL.props
+echo "Belbecs/Képesség/Memória 20" >> City/Debrecen/Sport/Esport/DEAC-Hackers/Verseny/LoL/LoL.props
+echo "Belbecs/Képesség/Képzelőerő 14" >> City/Debrecen/Sport/Esport/DEAC-Hackers/Verseny/LoL/LoL.props
+echo "Belbecs/Képesség/Kommunikáció 80" >> City/Debrecen/Sport/Esport/DEAC-Hackers/Verseny/LoL/LoL.props
+
+mkdir City/Debrecen/Sport/Esport/DEAC-Hackers/Verseny/LoL/ESL
+mkdir City/Debrecen/Sport/Esport/DEAC-Hackers/Verseny/LoL/ESL/LoL\ 5on5\ Summoners\'\ Arena
+
+echo "Külcsín/Kor/min 16" >> City/Debrecen/Sport/Esport/DEAC-Hackers/Verseny/LoL/ESL/LoL\ 5on5\ Summoners\'\ Arena/LoL\ 5on5\ Summoners\'\ Arena.props
+echo "Belbecs/Tapasztalat/Verseny rutin 10" >> City/Debrecen/Sport/Esport/DEAC-Hackers/Verseny/LoL/ESL/LoL\ 5on5\ Summoners\'\ Arena/LoL\ 5on5\ Summoners\'\ Arena.props
